c98943c6d16e63c79c65927248889d9a79f70429
Erik Brockbank <erikbrockbank@eriks-mbp.attlocal.net>
2020-02-19 18:23:47 -0800

    add bot strategies for transitions, clean up classes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * 3. in browser, visit http://localhost:3000/index.html
  *    -> to run a test version, visit http://localhost:3000/index.html?&mode=test
  *    -> to run a bot version, visit http://localhost:3000/index.html?&ver=2
- *    -> to view status of gameServer, visit http://localhost:3000/admin
+ *    -> to view status of game_server, visit http://localhost:3000/admin
  */
 
 
@@ -22,7 +22,7 @@ var app = require("express")(); // initialize express server
 var server = app.listen(3000); // listen on port 3000 (nginx will proxy requests on 80 to 3000)
 var io = require("socket.io").listen(server); // initialize socket.io
 var UUID = require("uuid"); // UUID library for generating unique IDs
-var gameServer = require(__dirname + JSPATH + "/" + "game.js"); // object for keeping track of games
+var game_server = require(__dirname + JSPATH + "/" + "game.js"); // object for keeping track of games
 
 
 // Initializing server
@@ -31,7 +31,7 @@ app.get("/*", function(req, res) {
     var file = req.params[0];
     if (file == "admin") { // admin endpoint returns state of game server for quick debugging
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(gameServer.getState(), null, 3));
+        res.end(JSON.stringify(game_server.getState(), null, 3));
     } else {
         res.sendFile(__dirname + "/" + file);
     }
@@ -46,41 +46,41 @@ io.on("connection", function (client) {
     initializeClient(client);
 });
 
-// Function to handle socket interactions between gameServer and clients
+// Function to handle socket interactions between game_server and clients
 initializeClient = function(client) {
     // extract relevant info from client request
     var istest = client.handshake.query.istest == "true";
     var version = client.handshake.query.version;
     // assign client to an existing game or start a new one
-    gameServer.findGame(client, version, istest);
+    game_server.findGame(client, version, istest);
 
     // handle player move submissions
     client.on("player_move", function(data) {
         console.log("app.js:\t detected player move: ", data);
-        gameServer.processMove(client, data);
+        game_server.processMove(client, data);
     });
 
     // handle player signal that they're ready for the next round
     client.on("player_round_complete", function(data) {
         console.log("app.js:\t detected player round complete");
-        gameServer.nextRound(client, data);
+        game_server.nextRound(client, data);
     });
 
     // handle player submitting exit survey free response data
     client.on("free_response_submit", function(data) {
         console.log("app.js:\t detected player free response submission");
-        gameServer.writeFreeRespData(data);
+        game_server.writeFreeRespData(data);
     });
 
     // handle player submitting exit survey slider data
     client.on("slider_submit", function(data) {
         console.log("app.js:\t detected player Likert slider submission");
-        gameServer.writeSliderData(data);
+        game_server.writeSliderData(data);
     });
 
     // handle disconnect
     client.on("disconnect", function() {
         console.log("app.js:\t detected client disconnect");
-        gameServer.clientDisconnect(client);
+        game_server.clientDisconnect(client);
     });
 };
